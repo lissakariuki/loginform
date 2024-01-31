152986a474c16664ddd0f876c9ffc42139979dc2
lissakariuki <melgaka1106@gmail.com>
2024-01-31 23:34:33 +0300

    comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const wrapper = document.querySelector('.wrapper');
-
+//function
 function activeLogIn() {
     wrapper.classList.add('activeLogIn');
     wrapper.classList.remove('activeSignUp');
 }
-
+//function
 function activeSignUp() {
     wrapper.classList.add('activeSignUp');
     wrapper.classList.remove('activeLogIn');
